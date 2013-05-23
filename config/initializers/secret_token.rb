3f2e5660c8bf935107c33cae4c7dfe058ea0c718
Denny <dennybritz@gmail.com>
2013-05-23 15:20:00 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3CorsUploadRails::Application.config.secret_token = 'a7e7d65ebe3fddd3b581e69d5c8a63b189bdd64710a5764528b9f8b9e1799c3fe302a5242fb4d55feb94bf7b24bf77c0d18d4c5842930390128603d76713795b'
